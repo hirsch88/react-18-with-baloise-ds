5af156e8c5d393641a2d5f98220272fdd5fd24e0
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-03-06 10:08:02 +0100

    add DS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  useBaloiseDesignSystem,
+  BalApp,
+  BalHeading,
+  BalButton,
+  BalFooter,
+} from "@baloise/design-system-components-react";
 
 function App() {
+  useBaloiseDesignSystem();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BalApp className="has-sticky-footer">
+      <header></header>
+      <main className="container">
+        <BalHeading>Hello World!</BalHeading>
+        <BalButton>Button</BalButton>
+      </main>
+      <BalFooter></BalFooter>
+    </BalApp>
   );
 }
 
